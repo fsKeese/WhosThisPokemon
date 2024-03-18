7cef045214ac388034cbdceb852c50e5944b668c
FSKeese <basti.keese@googlemail.com>
2024-03-18 20:01:56 +0100

    Pokemon class and sound

--- lit.py
+++ lit.py
@@ -4,9 +4,16 @@ import numpy as np
 import io
 from io import BytesIO
 from PIL import Image
+from playsound import playsound
 
 import streamlit as st
 from utils import *
+
+
+
+if "pokemon_id" not in st.session_state:
+    st.session_state.pokemon_id = 1
+
 
 st.write("""
          Is this Ditto?
@@ -14,11 +21,27 @@ st.write("""
 
 
 
+# if st.button("Next"):
+#     pokemon_id = np.random.randint(0, 1025) ## ALLLLL POKEEEE
+#     img = grab_poke(url="https://pokeapi.co/api/v2/pokemon/", poke_number=pokemon_id)
+#     img, solution = turn_black_resize(img)
+#     result = create_whos_poke(img)
+#     st.image(result)
+
+# if st.button("Result"):
+#     result_sol = create_whos_poke(solution)
+#     st.image(result_sol)
+
 if st.button("Next"):
-    pokemon_id = np.random.randint(0, 1025) ## ALLLLL POKEEEE
-    img = grab_poke(url="https://pokeapi.co/api/v2/pokemon/", poke_number=pokemon_id)
-    img = turn_black_resize(img)
-    result = create_whos_poke(img)
-    st.image(result)
+    #st.audio("sounds\whoisthat.wav")
+    poke_id = np.random.randint(0, 1020)
+    st.session_state.pokemon_id = poke_id
+    poke = Pokemon(url="https://pokeapi.co/api/v2/pokemon/", background_path="whos_that_poke.png", id=st.session_state.pokemon_id)
+    st.image(poke.get_question())
+    playsound("sounds\whoisthat.wav")
 
+if st.button("Result"):
+    poke = Pokemon(url="https://pokeapi.co/api/v2/pokemon/", background_path="whos_that_poke.png", id=st.session_state.pokemon_id)
 
+    st.image(poke.get_solution())
+    playsound("sounds\its.wav")
--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def turn_black_resize(img):
     poke_numpy[height, width] = 0 ## turn Pokemon black
     poke_numpy = poke_numpy[np.min(height): np.max(height), np.min(width):np.max(width)] ## crop
     poke = Image.fromarray(poke_numpy)
-    return poke
+    return poke, img.resize((350,350), Image.Resampling.NEAREST) ## return original as well
 
 
 def create_whos_poke(img):
@@ -41,3 +41,76 @@ def create_whos_poke(img):
     anker = (405- int(resized_height/2), int(394-(resized_width/2)))
     who.paste(img, anker)
     return who
+
+
+class Pokemon:
+    def __init__(self, url="https://pokeapi.co/api/v2/pokemon/", background_path="whos_that_poke.png", id=9):
+        self.id = id
+        self.pokemon_json = requests.get(url+str(self.id)).json()
+        self.background_path = background_path
+        
+        self.sprite = self.pokemon_json["sprites"]["front_default"]
+    
+    def load_pokemon_image(self):
+        image_io = requests.get(self.sprite).content
+        img = Image.open(io.BytesIO(image_io)).convert("RGB")
+        return img
+        
+    def turn_black(self):
+        img = self.load_pokemon_image()
+        
+        gray_image = img.convert("L") ## turn image gray
+        gray_image_numpy = np.copy(np.asarray(gray_image)) ## copy to prevent only read rights
+        height, width = np.where(gray_image_numpy ==0) 
+        gray_image_numpy[height, width] = 255
+        
+        poke = Image.fromarray(gray_image_numpy)
+        poke = poke.resize((350,350), Image.Resampling.NEAREST) ## resize image to fit into backgroung
+        
+        poke_numpy = np.copy(np.asarray(poke)) 
+        height, width = np.where(poke_numpy <255)
+        poke_numpy[height, width] = 0 ## turn Pokemon black
+        poke_numpy = poke_numpy[np.min(height): np.max(height), np.min(width):np.max(width)] ## crop
+        poke = Image.fromarray(poke_numpy)
+        return poke
+    
+    def turn_boarder_white(self):
+        original_img = self.load_pokemon_image()
+        
+        gray_image = original_img.convert("L") ## turn image gray
+        gray_image_numpy = np.copy(np.asarray(gray_image)) ## copy to prevent only read rights
+        height, width = np.where(gray_image_numpy ==0) 
+        
+        np_image = np.copy(np.asarray(original_img))
+        np_image[height, width, :] = 255
+        #np_image = np_image[np.min(height): np.max(height), np.min(width):np.max(width)] ## crop
+        
+        poke = Image.fromarray(np_image)
+        poke = poke.resize((350,350), Image.Resampling.NEAREST) ## resize image to fit into backgroung
+        return poke
+    
+    
+    def get_question(self):
+        img = self.turn_black()
+        background = Image.open(self.background_path)
+        height, width, channel = np.asarray(background).shape
+        who = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+        who.paste(background)
+        resized_height, resized_width = img.size
+        anker = (405- int(resized_height/2), int(394-(resized_width/2)))
+        who.paste(img, anker)
+        return who
+    
+    def get_solution(self):
+        img = self.turn_boarder_white()
+        
+        background = Image.open(self.background_path)
+        height, width, channel = np.asarray(background).shape
+        who = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+        who.paste(background)
+        resized_height, resized_width = img.size
+        anker = (405- int(resized_height/2), int(394-(resized_width/2)))
+        who.paste(img, anker)   
+        return who     
+        
+        
